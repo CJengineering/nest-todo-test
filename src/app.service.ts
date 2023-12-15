b47e9f7c11ed305c0e9241d93d67bf2efd2e8465
Tim Spiridonov <timour.spiridonov0gmail.com>
2023-12-15 13:21:37 +0100

    version 5 automate deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! fro; autotic deploy';
+    return 'Hello World! from autotic deploy';
   }
 }
